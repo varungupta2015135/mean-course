808810d73349cb09729fbd8857e2f3b13d6282b2
Gupta <Varun.Gupta3@Honeywell.com>
2019-03-28 09:55:13 +0530

    Created first component

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-post-create',
+    templateUrl: './post-create.component.html'
+})
+export class PostCreateComponent{
+    enteredValue = "";
+    newPost = "No Content!";
+
+    onAddPost(postInput: HTMLTextAreaElement){
+        this.newPost = this.enteredValue;
+    }
+}
