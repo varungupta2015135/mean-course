7476cecfcde63cffb6603b6156057289d566c638
Gupta <Varun.Gupta3@Honeywell.com>
2019-03-28 11:17:49 +0530

    Styled the form, added the toolbar and added post adding

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-post-create',
-    templateUrl: './post-create.component.html'
+    templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent{
-    enteredValue = "";
-    newPost = "No Content!";
+    enteredTitle = "";
+    enteredContent = "";
+    @Output() postCreated = new EventEmitter();
 
-    onAddPost(postInput: HTMLTextAreaElement){
-        this.newPost = this.enteredValue;
+    onAddPost(){
+        const post = {
+            title: this.enteredTitle, 
+            content: this.enteredContent
+        };
+        this.postCreated.emit(post);
     }
 }
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent{
+    // posts = [
+    //     {title: 'First Post', content: "This is the first posts's content"},
+    //     {title: 'Second Post', content: "This is the second posts's content"},
+    //     {title: 'Third Post', content: "This is the third posts's content"},
+    //     {title: 'Fourth Post', content: "This is the fourth posts's content"},
+    // ];
+    @Input() posts=[];
+}
